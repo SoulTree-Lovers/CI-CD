be72074217f1dfb150d9b2646f8aa6ea8ad330c8
kangseungmin <rkdtmdals999@naver.com>
2024-06-26 20:09:35 +0900

    feat: Health Check API 추가

---
+++ src/main/java/com/example/beanstalk/controller/HealthController.java
@@ -0,0 +1,16 @@
+package com.example.beanstalk.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class HealthController {
+
+    @GetMapping("/health")
+    public String healthCheck() {
+        return "Health OK";
+    }
+
+}
